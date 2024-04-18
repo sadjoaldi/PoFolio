753f9374960ccfd5ae8f2d1eaa5082ac206b3015
sadjoaldi <alsannealdi@hotmail.com>
2024-04-19 00:46:22 +0200

    ajout dossiers json, images et fichiers html,css

---
+++ JSON/language.js
@@ -0,0 +1,41 @@
+const language = [
+  {
+    Role: "FR",
+    content: {
+      Tabs: {
+        About: "À propos",
+        Projects: "Projets",
+        Contact: "Contact",
+      },
+      sectionAbout: {
+        Resume:
+          " Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates rerum obcaecati eveniet nemo enim ex tempora est, consectetur distinctio itaque. Ut recusandae totam architecto nemo quod laboriosam necessitatibus magnam voluptatem at vero earum iusto deleniti enim quibusdam distinctio corrupti fugit rerum iste, tempora minus. Iste facere ducimus voluptates adipisci, fugit ipsum! Doloremque veniam, et tempora dolor ut iusto! Enim eius nam necessitatibus aliquam dignissimos eaque harum beatae veritatis repudiandae neque.",
+
+        Extended: "En savoir plus",
+      },
+      sectionContact: {
+        Message: "Vous avez un projet en tête ?",
+        ButtonText: "Contactez-moi",
+      },
+    },
+  },
+  {
+    Role: "EN",
+    content: {
+      Tabs: {
+        About: "About",
+        Projects: "Projects",
+        Contact: "Contact",
+      },
+      SectionAbout: {
+        Resume:
+          " Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates rerum obcaecati eveniet nemo enim ex tempora est, consectetur distinctio itaque. Ut recusandae totam architecto nemo quod laboriosam necessitatibus magnam voluptatem at vero earum iusto deleniti enim quibusdam distinctio corrupti fugit rerum iste, tempora minus. Iste facere ducimus voluptates adipisci, fugit ipsum! Doloremque veniam, et tempora dolor ut iusto! Enim eius nam necessitatibus aliquam dignissimos eaque harum beatae veritatis repudiandae neque.",
+        Extended: "",
+      },
+      SectionContact: {
+        Message: "Have a question or want to work together?",
+        ButtonText: "Getting in touch",
+      },
+    },
+  },
+];
